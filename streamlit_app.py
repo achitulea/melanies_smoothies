a58e779af5e863f7d3022fafe0ba04f7c0cfd317
Adrian Chitulea <48675210+achitulea@users.noreply.github.com>
2024-05-21 17:12:30 +0300

    Update streamlit_app.py
    Add requests to call an API

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,8 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + ' !', icon="✅")
 
+# Add requests for API calls
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
-# Next steps...
